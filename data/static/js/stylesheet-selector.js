53bea052bdfffad994381e62b0d5702c7aaefff7
Titus Studios Media Group <titusstudiosmediagroup@gmail.com>
2020-06-02 14:01:50 +1200

    Create stylesheet-selector.js

---
+++ data/static/js/stylesheet-selector.js
@@ -0,0 +1,20 @@
+$(document).on('pagebeforeshow', function () {
+  var URL = $.mobile.path.parseUrl(window.location).toString().toLowerCase();
+  
+  if (URL.indexOf("/commissions/voodoo") > -1) {
+    var newCss = document.createElement('link');
+    newCss.rel = 'stylesheet';
+    newCss.type = 'text/css';
+    newCss.href = '/data/static/scss/stylesheets/sourcerer-stylesheet.css';
+
+    document.body.appendChild(newCss);
+  }
+  else {
+    var newCss = document.createElement('link');
+    newCss.rel = 'stylesheet';
+    newCss.type = 'text/css';
+    newCss.href = '/assets/css/style.css';
+
+    document.body.appendChild(newCss);
+  }
+});
