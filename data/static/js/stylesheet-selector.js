feb9e7029c68b81f214f464947294973ff81f7b8
Titus Studios Media Group <titusstudiosmediagroup@gmail.com>
2020-06-02 14:32:56 +1200

    Update stylesheet-selector.js

--- data/static/js/stylesheet-selector.js
+++ data/static/js/stylesheet-selector.js
@@ -1,20 +1,26 @@
 $(document).on('pagebeforeshow', function () {
+  console.log("stylesheet function running")
   var URL = $.mobile.path.parseUrl(window.location).toString().toLowerCase();
+  console.log(URL)
   
   if (URL.indexOf("/commissions/voodoo") > -1) {
     var newCss = document.createElement('link');
     newCss.rel = 'stylesheet';
     newCss.type = 'text/css';
     newCss.href = '/data/static/scss/stylesheets/sourcerer-stylesheet.css';
+    console.log("Im on VooDoos Page")
 
     document.body.appendChild(newCss);
+    console.log("child appended")
   }
   else {
     var newCss = document.createElement('link');
     newCss.rel = 'stylesheet';
     newCss.type = 'text/css';
     newCss.href = '/assets/css/style.css';
+    console.log("Im NOT on VooDoos Page")
 
     document.body.appendChild(newCss);
+    console.log("child appended")
   }
 });
